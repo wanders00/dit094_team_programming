1d00f5c378e779f88aad9705ba9acd6e81daabef
William Andersson <williama00@hotmail.se>
2022-12-02 14:44:24 +0100

    Constructors and getters for color & difficulty

--- src/Color.java
+++ src/Color.java
@@ -1,9 +1,24 @@
 public class Color {
-    int red;
-    int green;
-    int blue;
+    private int red;
+    private int green;
+    private int blue;
 
-    Color() {
-        
+    Color(int red, int green, int blue) { // 0-255
+       this.red = red;
+       this.green = green;
+       this.blue = blue; 
+    }
+
+    // Either specific methods like this, or we can make one that returns an array of the 3 color codes.
+    public int getRedColorCode() {
+        return this.red;
+    }
+
+    public int getGreenColorCode() {
+        return this.green;
+    }
+
+    public int getBlueColorCode() {
+        return this.blue;
     }
 }
--- src/Difficulty.java
+++ src/Difficulty.java
@@ -1,5 +1,23 @@
 public enum Difficulty {
-    Easy,
-    Medium,
-    Hard;
+    Easy(1, 1),
+    Medium(1, 1),
+    Hard(1, 1);
+    // random numbers, change me later :)
+
+    private final double scoreMultiplier;
+    private final double speedMultiplier;
+
+    Difficulty(double scoreMultiplier, double speedMultiplier) {
+        this.scoreMultiplier = scoreMultiplier;
+        this.speedMultiplier = speedMultiplier;
+    }
+
+    public double getScoreMultiplier() {
+        return this.scoreMultiplier;
+    }
+
+    public double getSpeedMultiplier() {
+        return this.speedMultiplier;
+    }
+
 }
