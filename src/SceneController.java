558b08b68430a0bda034b7878aee5f145e80442b
marser <marser@student.chalmers.se>
2022-12-20 12:22:46 +0000

    Update src/SceneController.java

--- src/SceneController.java
+++ src/SceneController.java
@@ -1,7 +1,46 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class SceneController {
+public class SceneController extends Main {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToSettingsScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SettingsScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+    public void switchToMainScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("mainMenuScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchGameScene(ActionEvent event) throws IOException {
+        showStartGameScene();
+        //stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        //scene = new Scene(root);
+        //stage.setScene(scene);
+        //stage.show();
+    }
+
+    public void exitGameButton(ActionEvent event) throws IOException {
+        Platform.exit();
+    }
+}
