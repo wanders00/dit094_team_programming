5364d2d4bde371959aa5468eba3bc6cf69bdcc96
Marcus Serneblad <marser@student.chalmers.se>
2022-12-30 15:07:53 +0100

    Added usernameTextField in introScene to be able to get username input

--- src/Scenes/IntroScene.java
+++ src/Scenes/IntroScene.java
@@ -1,11 +1,18 @@
 package Scenes;
 import java.io.IOException;
+
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class IntroScene extends SceneController{
+
+    @FXML
+    private TextField userNameField;
+
     public void show(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/introScene.fxml"));
         stage.setTitle("Snake Game");
