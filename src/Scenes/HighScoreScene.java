26a53eb9459396340066f2fc4c5446291ddcd983
Marcus Serneblad <marser@student.chalmers.se>
2022-12-30 13:30:08 +0100

    updated highscorescene(not final)

--- src/Scenes/HighScoreScene.java
+++ src/Scenes/HighScoreScene.java
@@ -1,16 +1,21 @@
 package Scenes;
 
 import java.io.IOException;
+
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HighScoreScene extends SceneController {
+
+
     public void show(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/highScoreScene.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    
     }
 }
